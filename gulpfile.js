e3d9167800f71255c4fa116cc58ba4eb55f6041c
Meggy Barbier <meggybarbier@gmail.com>
2020-10-06 15:26:41 -0400

    github deploy script added

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,8 @@ var browserSync = require('browser-sync').create();
 
 var imagemin = require("gulp-imagemin")
 
+var ghpages = require("gh-pages")
+
 sass.compiler = require('node-sass');
 
 function defaultTask(cb) {
@@ -61,6 +63,10 @@ gulp.task("watch", function() {
   gulp.watch("src/css/app.scss", gulp.series("sass"));
   gulp.watch("src/fonts/*", gulp.series("fonts"))
   gulp.watch("src/img/*", gulp.series("images"))
+})
+
+gulp.task("deploy", function() {
+  ghpages.publish("dist")
 })
 
 gulp.task('default', gulp.series("html", "sass", "fonts", "watch"));
